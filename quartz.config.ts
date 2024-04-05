8de434a02714e07607d02a4d19411ddf4367296a
Kiran Brahma <akbrahma@gmail.com>
2024-04-05 23:25:45 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kiranbrahma.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "published",
     theme: {
