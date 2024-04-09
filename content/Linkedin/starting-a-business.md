c801140b9102990da662917a7ac3bbba3aee3a83
Kiran Brahma <akbrahma@gmail.com>
2024-04-09 17:27:03 +0530

    update

--- content/Linkedin/starting-a-business.md
+++ content/Linkedin/starting-a-business.md
@@ -9,20 +9,20 @@ date: 2024-04-09
 draft: false
 ---
   
-So you've got a brilliant business idea and keen to build it on the side. Your basic product or service is ready and even obtained a few paying customers. Now most would jump straight to start a Private Limited form. But is this the best way forward?
+So you've got a brilliant [[a-simple-guide-for-validating-new-business-ideas|business idea and keen to build it]] on the side. Your basic product or service is ready and even obtained a few paying customers. Now most would jump straight to start a Private Limited form. But is this the best way forward?
  
 There's a simpler way to start:
 - Consider a proprietorship (for solo ventures) or a partnership (for a team of 2 or more)
 	- Easier to manage and tax-friendly.
 	- Profits you Withdraw are taxed once, against twice in Pvt Ltd.
-	- Leverage Section 44AD for an even further tax reduction
+	- Leverage Section 44AD<sup><a href="https://cleartax.in/s/freelance-professional-business-income">1</a></sup> for an even further tax reduction
 
 ![Starting your business](https://i.imgur.com/t4aqfFm.jpeg)
 
 - Avoid the hassle of Compliance and paperwork.
-	- If you are a director, your existing firm can label this "moonlighting."
+	- If you are a director, your existing firm can label this >"moonlighting."
 
-- You can avail most benefits
+- You can avail most benefits:
 	- GST Registration
 	- Utilise a trade name for your side hustle,
 	- PAN card for partnership firms. Proprietorship you use your PAN
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,7 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
+
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -7,8 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      
     },
   }),
 }
@@ -26,8 +27,41 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.RecentNotes({
+      title: "Latest Post",
+      limit: 2,
+      filter: (f) =>
+        f.slug!.startsWith("Blog/") && f.slug! !== "Blog/index" && !f.frontmatter?.noindex,
+      linkToMore: "Blog/" as SimpleSlug,
+      
+    
+    })
+  ),
+  Component.DesktopOnly(
+    Component.RecentNotes({
+      title: "Latest Book Summary",
+      limit: 1,
+      filter: (f) =>
+        f.slug!.startsWith("Books/") && f.slug! !== "Books/index" && !f.frontmatter?.noindex,
+      linkToMore: "Books/" as SimpleSlug,
+      
+    
+    }),
+  ),
+  Component.DesktopOnly(
+    Component.RecentNotes({
+      title: "Latest Linkedin Post",
+      limit: 2,
+      filter: (f) =>
+        f.slug!.startsWith("Linkedin/") && f.slug! !== "Linkedin/index" && !f.frontmatter?.noindex,
+      linkToMore: "Linkedin/" as SimpleSlug,
+      
+    
+    }),
+  ),
   ],
+  
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
